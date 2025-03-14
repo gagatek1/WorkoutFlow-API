98cea86515213b9b8c5037386a32cb7e414060c6
Jakub Gagat <jakub.gagat@outlook.com>
2025-03-14 22:52:25 +0100

    Include main router

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from mangum import Mangum
 
+from app.api.v1.router import router
 from app.core.database import Base, engine
 
 app = FastAPI(title="WorkoutFlow API", version="0.1.0")
@@ -8,3 +9,4 @@ app = FastAPI(title="WorkoutFlow API", version="0.1.0")
 handler = Mangum(app)
 
 Base.metadata.create_all(engine)
+app.include_router(router)
