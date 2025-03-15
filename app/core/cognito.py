7d6444a1f07c656181d4dbb0dda9f0dd92fbee81
Jakub Gagat <jakub.gagat@outlook.com>
2025-03-15 18:12:51 +0100

    Edit

--- app/core/cognito.py
+++ app/core/cognito.py
@@ -6,9 +6,16 @@ from os import getenv
 import boto3
 from dotenv import load_dotenv
 
-from app.schemas.auth import (UserChangePassword, UserConfirmForgotPassword,
-                              UserForgotPassword, UserLogout, UserRefreshToken,
-                              UserSignIn, UserSignUp, UserVerify)
+from app.schemas.auth import (
+    UserChangePassword,
+    UserConfirmForgotPassword,
+    UserForgotPassword,
+    UserLogout,
+    UserRefreshToken,
+    UserSignIn,
+    UserSignUp,
+    UserVerify,
+)
 
 load_dotenv()
 
