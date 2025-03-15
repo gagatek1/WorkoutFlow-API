7f2ac1459e7b748bfd1620dc80c2f7b47543b162
Jakub Gagat <jakub.gagat@outlook.com>
2025-03-15 14:34:17 +0100

    Add sign in endpoint

--- app/api/v1/endpoints/auth_endpoint.py
+++ app/api/v1/endpoints/auth_endpoint.py
@@ -3,7 +3,8 @@ from starlette import status
 
 from app.core.cognito import Cognito
 from app.core.dependencies import get_cognito
-from app.schemas.auth import UserSignUp, UserVerify
+from app.schemas.auth import UserSignIn, UserSignUp, UserVerify
+from app.services.auth.signin_service import signin_service
 from app.services.auth.signup_service import signup_service
 from app.services.auth.verify_service import verify_service
 
@@ -18,3 +19,8 @@ async def signup_user(user: UserSignUp, cognito: Cognito = Depends(get_cognito))
 @auth_router.post("/verify", status_code=status.HTTP_200_OK)
 async def verify_user(user: UserVerify, cognito: Cognito = Depends(get_cognito)):
     return verify_service(user, cognito)
+
+
+@auth_router.post("/signin", status_code=status.HTTP_200_OK)
+async def signin_user(user: UserSignIn, cognito: Cognito = Depends(get_cognito)):
+    return signin_service(user, cognito)
