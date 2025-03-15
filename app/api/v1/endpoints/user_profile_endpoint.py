2ea0316bb03f35fafef382d1efacbfe05d5e985f
Jakub Gagat <jakub.gagat@outlook.com>
2025-03-15 18:14:13 +0100

    Add update endpoint

--- app/api/v1/endpoints/user_profile_endpoint.py
+++ app/api/v1/endpoints/user_profile_endpoint.py
@@ -1,4 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
+from app.core.database import db_dependency
+from app.core.security import get_token
+from app.schemas.user_profile import UserProfile
+from app.services.user_profile.update_service import update_service
 
 user_profile_router = APIRouter(prefix="/profile", tags=["profile"])
+
+
+@user_profile_router.put("/update/{profile_id}")
+async def update_profile(
+    profile_id,
+    update_profile: UserProfile,
+    db: db_dependency,
+    user: dict = Depends(get_token),
+):
+    return update_service(profile_id, update_profile, db, user)
