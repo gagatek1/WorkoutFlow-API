538b1765d1679fb66460a988caadb9dd909c87b5
Jakub Gagat <jakub.gagat@outlook.com>
2025-03-15 17:40:54 +0100

    Edit sign up service

--- app/services/auth/signup_service.py
+++ app/services/auth/signup_service.py
@@ -3,12 +3,22 @@ from fastapi import HTTPException
 from fastapi.responses import JSONResponse
 
 from app.core.cognito import Cognito
+from app.models.user_profile import UserProfile
 from app.schemas.auth import UserSignUp
 
 
-def signup_service(user: UserSignUp, cognito: Cognito):
+def signup_service(user: UserSignUp, cognito: Cognito, db):
     try:
         response = cognito.user_signup(user)
+
+        create_user_profile_model = UserProfile(
+            first_name=user.first_name,
+            last_name=user.last_name,
+            user_id=response["UserSub"],
+        )
+
+        db.add(create_user_profile_model)
+        db.commit()
     except botocore.exceptions.ClientError as e:
         if e.response["Error"]["Code"] == "UsernameExistsException":
             raise HTTPException(
