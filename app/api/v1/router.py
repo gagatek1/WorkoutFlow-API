480bb2c104875bb02a960443edf8cd2c63ba276a
Jakub Gagat <jakub.gagat@outlook.com>
2025-03-15 18:14:58 +0100

    Add include user profile router

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from app.api.v1.endpoints.auth_endpoint import auth_router
+from app.api.v1.endpoints.user_profile_endpoint import user_profile_router
 
 router = APIRouter(prefix="/api/v1")
 router.include_router(auth_router)
+router.include_router(user_profile_router)
