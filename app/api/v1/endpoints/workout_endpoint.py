68ed8dd7e2ff62e7f7b0368fd3dcccf102d3d017
Jakub Gagat <jakub.gagat@outlook.com>
2025-03-16 12:09:16 +0100

    Add update endpoint

--- app/api/v1/endpoints/workout_endpoint.py
+++ app/api/v1/endpoints/workout_endpoint.py
@@ -4,6 +4,7 @@ from app.core.database import db_dependency
 from app.core.security import get_token
 from app.schemas.workout import Workout
 from app.services.workout.create_service import create_service
+from app.services.workout.update_service import update_service
 
 workout_router = APIRouter(prefix="/workout", tags=["workout"])
 
@@ -13,3 +14,10 @@ async def create_workout(
     data: Workout, db: db_dependency, user: dict = Depends(get_token)
 ):
     return create_service(data, db, user)
+
+
+@workout_router.put("/update/{workout_id}")
+async def update_workout(
+    workout_id, data: Workout, db: db_dependency, user: dict = Depends(get_token)
+):
+    return update_service(workout_id, data, db, user)
