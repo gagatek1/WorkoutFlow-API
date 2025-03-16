50bdd65f4eb664908d4d249fdabf5c8150c0f92a
Jakub Gagat <jakub.gagat@outlook.com>
2025-03-16 12:40:08 +0100

    Add delete endpoint

--- app/api/v1/endpoints/workout_endpoint.py
+++ app/api/v1/endpoints/workout_endpoint.py
@@ -1,13 +1,15 @@
 from fastapi import APIRouter, Depends
+from starlette import status
 
 from app.core.database import db_dependency
 from app.core.security import get_token
 from app.schemas.workout import Workout
 from app.services.workout.create_service import create_service
+from app.services.workout.delete_service import delete_service
 from app.services.workout.get_service import get_workout, get_workouts
 from app.services.workout.update_service import update_service
 
-workout_router = APIRouter(prefix="/workout", tags=["workout"])
+workout_router = APIRouter(prefix="/workouts", tags=["workouts"])
 
 
 @workout_router.post("/create")
@@ -32,3 +34,10 @@ async def show_workout(workout_id, db: db_dependency, user: dict = Depends(get_t
 @workout_router.get("/")
 async def show_workouts(db: db_dependency, user: dict = Depends(get_token)):
     return get_workouts(db, user)
+
+
+@workout_router.delete("/delete/{workout_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_workout(
+    workout_id, db: db_dependency, user: dict = Depends(get_token)
+):
+    delete_service(workout_id, db, user)
