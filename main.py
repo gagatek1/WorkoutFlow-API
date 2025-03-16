f243d1e51815319822ef4c578653912ddb5b42f9
Jakub Gagat <jakub.gagat@outlook.com>
2025-03-16 13:27:12 +0100

    Edit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from mangum import Mangum
 from app.api.v1.router import router
 from app.core.database import Base, engine
 
-app = FastAPI(title="WorkoutFlow API", version="0.1.0")
+app = FastAPI(title="WorkoutFlow API", version="0.3.0")
 
 handler = Mangum(app)
 
