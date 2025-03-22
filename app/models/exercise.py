f34ed79a9dd37318ab61f2cc1e9da07a7cd7bf5f
Jakub Gagat <jakub.gagat@outlook.com>
2025-03-22 12:57:41 +0100

    Edit model

--- app/models/exercise.py
+++ app/models/exercise.py
@@ -1,7 +1,19 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from enum import Enum as PyEnum
+
+from sqlalchemy import Column, Enum, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
 from app.core.database import Base
+
+
+class ExercisePart(PyEnum):
+    back = "back"
+    biceps = "biceps"
+    chest = "chest"
+    core = "core"
+    legs = "legs"
+    shoulders = "shoulders"
+    triceps = "triceps"
 
 
 class Exercise(Base):
@@ -11,6 +23,7 @@ class Exercise(Base):
     name = Column(String)
     sets = Column(Integer)
     reps = Column(Integer)
+    part = Column(Enum(ExercisePart), nullable=False)
     weight = Column(Integer)
     workout_id = Column(Integer, ForeignKey("workouts.id"), index=True, nullable=False)
     workout = relationship("Workout", back_populates="exercises")
