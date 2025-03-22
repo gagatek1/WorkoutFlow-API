c1276112cc8f4ab8e9984157460090142e569356
Jakub Gagat <jakub.gagat@outlook.com>
2025-03-22 13:16:52 +0100

    Add get endpoints

--- app/api/v1/endpoints/exercise_endpoint.py
+++ app/api/v1/endpoints/exercise_endpoint.py
@@ -4,6 +4,7 @@ from app.core.database import db_dependency
 from app.core.security import get_token
 from app.schemas.exercise import Exercise
 from app.services.exercise.create_service import create_service
+from app.services.exercise.get_service import get_exercise, get_exercises
 
 exercise_router = APIRouter(prefix="/exercises", tags=["exercises"])
 
@@ -13,3 +14,15 @@ async def create_exercise(
     data: Exercise, db: db_dependency, user: dict = Depends(get_token)
 ):
     return create_service(data, db, user)
+
+
+@exercise_router.get("/{exercise_id}")
+async def show_exercise(
+    exercise_id, db: db_dependency, user: dict = Depends(get_token)
+):
+    return get_exercise(exercise_id, db, user)
+
+
+@exercise_router.get("/")
+async def show_exercises(db: db_dependency, user: dict = Depends(get_token)):
+    return get_exercises(db, user)
