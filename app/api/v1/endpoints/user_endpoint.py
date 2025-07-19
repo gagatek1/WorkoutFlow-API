462ef36da12b3ca62a4e00ac22f5f0a9e562bf3a
gagatek1 <jakub.gagat@outlook.com>
2025-07-19 18:06:04 +0200

    Add new endpoints

--- app/api/v1/endpoints/user_endpoint.py
+++ app/api/v1/endpoints/user_endpoint.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter, Depends
 
 from app.core.cognito import Cognito
+from app.core.database import db_dependency
 from app.core.dependencies import get_cognito
-from app.schemas.user import UserEmail
-from app.services.user.update_service import get_token, update_email_service
+from app.core.security import get_token as security_get_token
+from app.schemas.user import User, UserEmail
+from app.services.user.get_service import get_service, me_service
+from app.services.user.update_email_service import (get_token,
+                                                    update_email_service)
+from app.services.user.update_service import update_service
 
 user_router = APIRouter(prefix="/users", tags=["users"])
 
@@ -15,3 +20,26 @@ async def change_email(
     user: dict = Depends(get_token),
 ):
     return update_email_service(data, user, cognito)
+
+
+@user_router.put("/")
+async def update_profile(
+    update_profile: User,
+    db: db_dependency,
+    cognito_user: dict = Depends(get_token),
+):
+    return update_service(update_profile, db, cognito_user)
+
+
+@user_router.get("/{user_id}")
+async def get_profile(
+    user_id, db: db_dependency, cognito_user: dict = Depends(security_get_token)
+):
+    return get_service(user_id, db)
+
+
+@user_router.get("/")
+async def get_my_profile(
+    db: db_dependency, cognito_user: dict = Depends(security_get_token)
+):
+    return me_service(db, cognito_user)
