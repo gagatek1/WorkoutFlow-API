9d3908d561e45c4ba005e315582954ca86e39624
gagatek1 <jakub.gagat@outlook.com>
2025-07-19 17:32:14 +0200

    Edit workout services

--- app/services/workout/create_service.py
+++ app/services/workout/create_service.py
@@ -1,22 +1,22 @@
 from fastapi import HTTPException
 
-from app.models.user_profile import UserProfile
+from app.models.user import User
 from app.models.workout import Workout
 from app.schemas.workout import Workout as CreateWorkout
 
 
-def create_service(data: CreateWorkout, db, user):
+def create_service(data: CreateWorkout, db, cognito_user):
     if data.name is None:
         raise HTTPException(status_code=422, detail="Name is none")
 
-    profile = (
-        db.query(UserProfile)
-        .filter(UserProfile.user_id == user.get("Username"))
+    user = (
+        db.query(User)
+        .filter(User.cognito_id == cognito_user.get("Username"))
         .first()
     )
-    new_workout = Workout(name=data.name, date=data.date, profile_id=profile.id)
+    new_workout = Workout(name=data.name, date=data.date, user_id=user.id)
 
-    profile.workout_quantity += 1
+    user.workout_quantity += 1
 
     db.add(new_workout)
     db.commit()
--- app/services/workout/delete_service.py
+++ app/services/workout/delete_service.py
@@ -1,21 +1,21 @@
 from fastapi import HTTPException
 
-from app.models.user_profile import UserProfile
+from app.models.user import User
 from app.models.workout import Workout
 
 
-def delete_service(workout_id, db, user):
+def delete_service(workout_id, db, cognito_user):
     workout = db.query(Workout).filter(Workout.id == workout_id).first()
-    profile = (
-        db.query(UserProfile)
-        .filter(UserProfile.user_id == user.get("Username"))
+    user = (
+        db.query(User)
+        .filter(User.cognito_id == cognito_user.get("Username"))
         .first()
     )
 
     if workout is None:
         raise HTTPException(status_code=404, detail="Not found")
-    if workout.profile_id == profile.id:
-        profile.workout_quantity -= 1
+    if workout.user_id == user.id:
+        user.workout_quantity -= 1
         db.delete(workout)
         db.commit()
     else:
